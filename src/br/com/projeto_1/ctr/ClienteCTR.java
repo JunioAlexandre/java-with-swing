972fc09bfbf8437e8aba54959189680c5be37f8a
JunioAlexandre <junioalexandre1337@gmail.com>
2025-02-26 22:41:42 -0300

    att project

---
+++ src/br/com/projeto_1/ctr/ClienteCTR.java
@@ -0,0 +1,46 @@
+package br.com.projeto_1.ctr;
+import java.sql.ResultSet;
+import br.com.projeto_1.dto.ClienteDTO;
+import br.com.projeto_1.dao.ClienteDAO;
+import br.com.projeto_1.dao.ConexaoDAO;
+
+public class ClienteCTR {
+    ClienteDAO clienteDAO = new ClienteDAO();
+    
+    
+    public ClienteCTR(){ 
+     
+      }
+    
+    public String inserirCliente(ClienteDTO clienteDTO){
+        try{
+            //Chama o metodo que esta na classe DAO aguardando uma resposta (true0 or (false)
+            if (clienteDAO.inserirCliente(clienteDTO)) {
+                return "Cliente cadastrado com sucesso!!";
+            }else {
+                return "Cliente não cadastrado!!!";
+            }
+        }//Caso tenha algum erro no codgo acima é enviado uma mensagem no
+        //console com o que esta acontecendo.
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Cliente não cadastrado";
+        }
+    }
+    
+    public ResultSet consultarCliente(ClienteDTO clienteDTO, int opcao){
+        //É criado um atributo do tipo ResultSet, pois este metodo receve o resultado de uma consulta
+        ResultSet rs = null;
+        
+        //O atributo rs recebe a consulta realizada pelo metodo da classe DAO
+        rs = clienteDAO.consultarCliente(clienteDTO, opcao);
+        
+        return rs;
+    }
+    
+    //Metodo utilizado para fechar o banco de dados
+    public void CloseDB(){
+        ConexaoDAO.CloseDB();
+    }
+    
+}
